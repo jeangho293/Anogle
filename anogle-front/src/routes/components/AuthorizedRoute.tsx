e02564c31a669533a63646b1582791858986b2cb
김정호 <jeangho293@naver.com>
2024-10-10 17:06:24 +0900

    feat: authorizedroutes 수정

--- anogle-front/src/routes/components/AuthorizedRoute.tsx
+++ anogle-front/src/routes/components/AuthorizedRoute.tsx
@@ -1,20 +1,14 @@
-import { useEffect } from "react";
 import { Stack } from "@mui/material";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { SideMenuBar } from "../../components";
+import { useUser } from "../../libs";
 
 function AuthorizedRoute() {
-  const navigator = useNavigate();
+  const [user] = useUser({ canBeUnauthenticated: true });
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-
-  //   if (!token) {
-  //     navigator("/sign-in");
-  //   }
-  // });
-
-  return (
+  return !user ? (
+    <Navigate to="/sign-in" replace />
+  ) : (
     <Stack direction="row">
       <Stack css={{ width: "100%", maxWidth: "240px" }}>
         <SideMenuBar />
